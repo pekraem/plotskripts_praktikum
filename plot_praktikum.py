98e00cd036e845ee6af5b66ef6c1cae8ce086fc3
pekraem <upcnb@student.kit.edu#>
2016-12-07 00:16:05 +0100

    fits added, initialisation of new plotclass

--- plot_praktikum.py
+++ plot_praktikum.py
@@ -2,6 +2,50 @@ from ROOT import *
 from array import array
 import numpy as np
 
+
+class plot:
+    def __init__(self, name="Plot", path, typ="graph", fits=[], color=kBlack, outfile="_plot.pdf"):
+        """Initialise a new instance of 'plot' class with attributes:
+        -name: name and title of the plot
+        -path ist a string with path of data, to plot more than one dataset in the same plot, use 'addPath'
+        -typ can be graph (TGraphErrors), hist (TH1F - normaly filled), hist_bin (TH1F - set bin by bin)
+        -fit is a list with all fits, shape: [["type",xmin="",xmax=""],[...]...], better use 'addFit'
+        -color can be used to change the color of the datapoints"""
+        self.name=name
+        if path!='':
+            self.path=path
+        else:
+            print "no plot without path!"
+        self.typ=typ
+        self.fits=fits
+        self.color=color
+        self.outfile=self.name+outfile
+        self.multiData=False                #flag for multiple Datasets
+    
+    def addPath(self, pathstr):
+        """sets flag multiData=True, converts self.path to list with path and adds new pathstring to this list"""
+        if multidata==False:
+            self.multiData=True
+            self.path=[self.path]
+        self.path.append(pathstr)
+        
+    def addFit(self, ftyp="lin", fxmin=-999, fxmax=999, findex=0):
+        """adds fit to plot, ftyp can be 'lin' for a linear fit or 'gaus' for gaussian fit. fxmin and fxmax sets fitting range.
+        findex is only used for multiData plots and points to the dataset"""
+        self.fit.append([ftyp,fxmin,fxmax,findex])
+        
+    def setName(self,name):
+        self.name=name
+        
+    def setTyp(self, typ):
+        self.typ=typ
+    
+    def setOutfile(self, outpath):
+        self.outfile=outpath
+        
+    
+        
+
 def data_2_array(filepaths):
     """reads given files with messured values (x and y) and creates lists with arrays. filepaths=["path1","path2",...]"""
     x_arraylist=[]
@@ -57,3 +101,20 @@ def draw_graphs(graphlist):
     for graph in graphlist:
         graph.Draw("AP SAME")
     raw_input()
+    
+
+def fit_gaus(data, x_min, x_max):
+    """fits a gaussian distribution to a given data set. data must be a fitable ROOTObject (TH1F or TGraphErrors etc). x_min and x_max are cuts on the fitrange, returns the fitfunction, mean and error"""
+    fkt = TF1("fkt","gaus",x_min,x_max)
+    data.Fit("fkt","R")
+    mean = fkt.GetParameter(1)
+    error = fkt.GetParError(1)
+    return fkt, mean, error
+
+def fit_lin(data, x_min, x_max):
+    """fits a linear slope to a given data set. data must be a fitable ROOTObject (TH1F or TGraphErrors etc). x_min and x_max are cuts on the fitrange, returns the fitfunction, gradient and error"""
+    fkt = TF1("fkt","[0]*x+[1]",x_min,x_max)
+    grad = fkt.GetParameter(0)
+    error = fkt.GetParamError(0)
+    data.fit("fkt","R")
+    return fkt, grad, error
